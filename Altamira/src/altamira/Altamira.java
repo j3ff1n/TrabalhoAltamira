dd5f045b15314b6413693e1f7786eae298818853
j3ff1n <j3ff1n@Jefferson>
2017-07-31 21:29:18 -0300

    NOVA ALTERAÇÃO

--- Altamira/src/altamira/Altamira.java
+++ Altamira/src/altamira/Altamira.java
@@ -15,7 +15,8 @@ public class Altamira {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-System.out.println("trabalho Altamira");
+        System.out.println("trabalho Altamira");
+        System.out.println("OLÁ PROFESORA ALTAMIRA");
     }
     
 }
